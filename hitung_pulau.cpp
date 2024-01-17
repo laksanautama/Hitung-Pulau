7c34c447183fec432c253bd3e7221549c570256c
laksanautama <kussa_laksana@uhnsugriwa.ac.id>
2024-01-17 10:29:37 +0800

    submit file cpp and pdf

---
+++ hitung_pulau.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+//---dilarang menambah kode di atas batas ini-----
+
+int hitung_pulau(){
+
+}
+
+int main(){
+
+return 0;
+}
